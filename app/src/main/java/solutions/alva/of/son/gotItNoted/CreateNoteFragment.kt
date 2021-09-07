8a9b61f6f65fe21cdf983b695d95164904476096
Daniel Alvarez <70470536+djag07907@users.noreply.github.com>
2021-09-07 10:37:49 -0600

    add notes UI

---
+++ app/src/main/java/solutions/alva/of/son/gotItNoted/CreateNoteFragment.kt
@@ -0,0 +1,32 @@
+package solutions.alva.of.son.gotItNoted
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CreateNoteFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            CreateNoteFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
